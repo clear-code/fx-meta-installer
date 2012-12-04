6d4b4fe160b9d11c9d4762be0e24f04be86bf063
mooz <stillpedant@gmail.com>
2012-12-04 12:50:35 +0900

    Suppress the test command error that occurs when empty string is given
    Comparing "x"-prefixed strings is a well-know dirty hack for test
    commands

--- make.sh
+++ make.sh
@@ -106,7 +106,7 @@ done
 addon_files=$(echo "$addon_files" | $sed 's/^\|//')
 
 given_addon_files=$(read_config "INSTALL_ADDONS")
-if [ $given_addon_files != "" ]
+if [ "x${given_addon_files}" != "x" ]
 then
   addon_files=$given_addon_files
 fi
