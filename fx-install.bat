eb0d300d05bbab606ef28de44406dba6c4cab6b5
YUKI Hiroshi <shimoda@clear-code.com>
2013-12-20 12:29:08 +0900

    Disable echo

--- fx-install.bat
+++ fx-install.bat
@@ -1,4 +1,4 @@
-rem @echo off
+@echo off
 rem ==================================================================
 rem This Source Code Form is subject to the terms of the Mozilla Public
 rem License, v. 2.0. If a copy of the MPL was not distributed with this
