93abd7b9449a512f8b660a03d853c5f09ff42efe
YUKI Hiroshi <shimoda@clear-code.com>
2014-06-10 14:48:56 +0900

    Add skeleton

---
+++ fainstall.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Copyright (C) 2014 ClearCode Inc.
+
+
+# Enable/disable crash report
+
+# Update existing shortcuts
+
+# Set default client / disable client
+
+# Install custom profiles
+
+# Install additional files
+
+## Install *.cfg => appdir/
+## Install *.properties => appdir/
+## Install override.ini => appdir/
+
+## Install *.cer => appdir/defaults/
+## Install *.crt => appdir/defaults/
+## Install *.pem => appdir/defaults/
+## Install *.cer.override => appdir/defaults/
+## Install *.crt.override => appdir/defaults/
+## Install *.pem.override => appdir/defaults/
+
+## Install bookmarks.html => appdir/defaults/profile/
+## Install *.rdf => appdir/defaults/profile/
+
+## Install *.xml => appdir/isp/
+
+## Install *.js => appdir/defaults/pref or appdir/defaults/preferences/
+
+## Install *.css => appdir/chrome/
+## Install *.jar => appdir/chrome/
+## Install *.manifest => appdir/chrome/
+
+## Install *.xpt => appdir/components/
+
+## Install *.dll => appdir/plugins/
+
+## Install distribution.* => appdir/distribution/
+
+## for Firefox
+### Install override.ini => appdir/browser/
+### Install bookmarks.html => appdir/browser/defaults/profile/
+### Install *.rdf => appdir/browser/defaults/profile/
+### Install *.dll => appdir/browser/plugins/
+
+## for Netscape
+### Install installed-chrome.txt => appdir/
+
+## Install *.lnk => desktop/
+
+# Install addons
+
+# Install shortcuts
+
+# Run extra installers
+
+# Install search plugins
+
+## Install searchplugins => appdir/browser/searchplugins/ or appdir/searchplugins/
+
+## Disable searchplugins
