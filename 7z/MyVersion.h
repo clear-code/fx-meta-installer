e04ff04343eaf610e36e5a3f303d0d6e05834cc0
YUKI Hiroshi <shimoda@clear-code.com>
2014-10-29 19:13:14 +0900

    Update steps to build 7zip self-extraction module for the latest release

--- 7z/MyVersion.h
+++ 7z/MyVersion.h
@@ -1,8 +1,8 @@
-#define MY_VER_MAJOR 4
-#define MY_VER_MINOR 57
+#define MY_VER_MAJOR 9
+#define MY_VER_MINOR 20
 #define MY_VER_BUILD 0
-#define MY_VERSION "4.57"
-#define MY_7ZIP_VERSION "7-Zip 4.57"
-#define MY_DATE "2008-05-23"
-#define MY_COPYRIGHT "Copyright (c) 2008 ClearCode Inc."
+#define MY_VERSION "9.20"
+#define MY_7ZIP_VERSION "7-Zip 9.20"
+#define MY_DATE "2010-11-18"
+#define MY_COPYRIGHT "Copyright (c) 2014 ClearCode Inc."
 #define MY_VERSION_COPYRIGHT_DATE MY_VERSION "  " MY_COPYRIGHT "  " MY_DATE
