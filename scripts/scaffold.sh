55f10e730481613123ab6e1204cf55e1fbfb00c6
Masafumi Yokoyama <yokoyama@clear-code.com>
2016-02-12 10:57:20 +0900

    Copy templates for scaffold

--- scripts/scaffold.sh
+++ scripts/scaffold.sh
@@ -14,3 +14,7 @@ add_blank_dir resources
 add_blank_dir installer-config
 
 cp "${base_dir}/templates/Makefile" "${project_root}"
+cp "${base_dir}/templates/installer-config/config.bat" "${project_root}"/installer-config
+cp "${base_dir}/templates/installer-config/config.nsh" "${project_root}"/installer-config
+cp "${base_dir}/templates/installer-config/fainstall.ini" "${project_root}"/installer-config
+cp "${base_dir}/templates/resources/Firefox-setup.ini" "${project_root}"/resources
